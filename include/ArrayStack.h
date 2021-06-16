edbd45859d44def724ff436ee8242b05b18fa2b8
Arya Shakya <ryaashakya@gmail.com>
2021-06-16 16:33:46 +0545

    Declared all functions in header file

--- include/ArrayStack.h
+++ include/ArrayStack.h
@@ -1,27 +1,92 @@
+#include <iostream>
+
 template <typename T>
 class Stack
 {
 public:
-    virtual bool isEmpty() = 0;
-    virtual bool isFull() = 0;
-    virtual T push(const T &element);
-    virtual T pop() = 0;
-    virtual T top() = 0;
+    virtual bool isEmpty() const = 0;
+    virtual bool isFull() const = 0;
+    virtual bool push(const T &element) = 0;
+    virtual bool pop(T &element) = 0;
+    virtual bool top(T &element) const = 0;
 };
 
 template <typename T>
 class ArrayStack : public Stack<T>
 {
 public:
-    ArrayStack(int size);
-    bool isEmpty() = 0;
-    bool isFull() = 0;
-    T push(const T &element);
-    T pop();
-    T top();
+    ArrayStack(int maxStackSize);
+    bool isEmpty() const;
+    bool isFull() const;
+    bool push(const T &element);
+    bool pop(T &element);
+    bool top(T &element) const;
 
 protected:
     T *data;
     int topIndex;
-    int size;
+    int maxStackSize;
+};
+
+template <typename T>
+ArrayStack<T>::ArrayStack(int maxStackSize) : maxStackSize(maxStackSize),
+                                              data(new T[maxStackSize]),
+                                              topIndex(-1){};
+
+template <typename T>
+bool ArrayStack<T>::isEmpty() const
+{
+    return topIndex < 0;
+};
+
+template <typename T>
+bool ArrayStack<T>::isFull() const
+{
+    return (maxStackSize - 1) == topIndex;
+};
+
+template <typename T>
+bool ArrayStack<T>::push(const T &element)
+{
+    if (!isFull())
+    {
+        topIndex++;
+        data[topIndex] = element;
+        return true;
+    }
+    else
+    {
+        std::cout << "Stack is full\n";
+        return false;
+    }
+};
+
+template <typename T>
+bool ArrayStack<T>::pop(T &element)
+{
+    if (!isEmpty())
+    {
+        element = data[topIndex--];
+        return true;
+    }
+    else
+    {
+        std::cout << "Stack is Empty\n";
+        return false;
+    }
+};
+
+template <typename T>
+bool ArrayStack<T>::top(T &element) const
+{
+    if (!isEmpty())
+    {
+        element = data[topIndex];
+        return true;
+    }
+    else
+    {
+        std::cout << "Stack is Empty\n";
+        return false;
+    }
 };
